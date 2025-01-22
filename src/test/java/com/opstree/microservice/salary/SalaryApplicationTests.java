1716124ad3395c33870c9f2f79cf24526d06bb7d
ShivaniNarula29 <shivaninarula9211@gmail.com>
2025-01-22 16:49:47 +0530

    Update SalaryApplicationTests.java

--- src/test/java/com/opstree/microservice/salary/SalaryApplicationTests.java
+++ src/test/java/com/opstree/microservice/salary/SalaryApplicationTests.java
@@ -6,7 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class SalaryApplicationTests {
 
-	@Test
+	@Test(classes =SalaryApplicationTests.class)
 	void contextLoads() {
 	}
 
