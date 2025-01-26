c08a84a4bad5cd77007038c9fafda56d98af7947
ShivaniNarula29 <shivaninarula9211@gmail.com>
2025-01-27 02:26:15 +0530

    Update myPipeline.groovy

--- vars/myPipeline.groovy
+++ vars/myPipeline.groovy
@@ -11,7 +11,6 @@ def call() {
                 script {
                     // Call the checkoutRepo function with repository URL and branch
                    checkoutRepo('https://github.com/ShivaniNarula29/CICD-01.git', 'main')   // Checkout code using the function from checkoutStage.groovy
-                   compileStage()    // Compile code using the function from compileStage.groovy
                 }
             }
           }
