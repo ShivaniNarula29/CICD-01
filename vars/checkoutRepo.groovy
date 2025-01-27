efebeea01d1f41913fb17e152bf1a48b5935bafc
ShivaniNarula29 <shivaninarula9211@gmail.com>
2025-01-27 08:35:14 +0530

    Update checkoutRepo.groovy

--- vars/checkoutRepo.groovy
+++ vars/checkoutRepo.groovy
@@ -2,9 +2,9 @@
 
 def call(String repoUrl, String branch = 'main') {
     echo "Checking out repository from ${repoUrl} with branch ${branch}"
-// if (repoUrl == '') {
-//         error "Repository URL is required"
-//     }
+if (repoUrl == '') {
+        error "Repository URL is required"
+}
     // Checkout the GitHub repository using the Git plugin
     git branch: branch, url: repoUrl
 }
